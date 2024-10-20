ba22efec765dbdbff40157a5f1d137ffdbc4af40
Southern Just <117814631+Southern-Just@users.noreply.github.com>
2024-10-21 01:50:13 +0300

    fetching data from api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,40 +1,58 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
+import { Link } from 'react-router-dom'
 
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
 
-
-const TitleCards = ({title, category}) => {
-
-    const cardsRef = useRef();
-
-    const handleWheel = (event) =>{
-        event.scroll();
-        cardsRef.current.scrollLeft += event.deltaY;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjRkZTMxNjlkOWNhZjgxNWI5NWJlNDBmNTMwNjAzNyIsIm5iZiI6MTcyOTM2NzEwMC40MTYzNzcsInN1YiI6IjY3MTQwYjE4NjUwMjQ4YjlkYjYyM2E4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XpengJaEaB3LbGeFnHNOAM95IGWKfmg0QgBPk4ak6Go'
     }
+  };
 
+  const handleWheel = (event) => {
+    event.preventDefault()
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
 
-    useEffect(()=>{
-        cardsRef.current.addEventListener('wheel', handleWheel)
-    },[])
-    
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+      .then(response => response.json())
+      .then(response => setApiData(response.results))
+      .catch(err => console.error(err));
+
+    const currentRef = cardsRef.current;
+    currentRef.addEventListener('wheel', handleWheel);
+
+    return () => {
+      currentRef.removeEventListener('wheel', handleWheel);
+    };
+  }, [category]);
+
 
   return (
     <div className='title-cards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index)=>{
-            return(
-                <div className="card" key={index}>
-                    <img src={card.image} alt="" />
-                    <p>{card.name}</p>
-                </div>
-            )
-
-        })}
+        {apiData && apiData.length > 0 ? (
+          apiData.map((card, index) => (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt={card.original_title} />
+              <p>{card.original_title}</p>
+            </Link>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
-  )
+  );
+  
 }
 
-export default TitleCards
+export default TitleCards;
